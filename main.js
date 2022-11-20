e58b4f182b6855b6b6e70318ea1c9c1287bdb010
JP-Borgo <juanpabloborgogil@hotmail.com>
2022-11-19 17:46:38 -0700

    javaScript Assignment Completed part 2

--- main.js
+++ main.js
@@ -27,42 +27,4 @@ function onSubmit(e){
         nameInput.value = '';
         emailInput.value = '';
     }
-};
-
-/*
-//console.log(window)
-
-// Single element
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-console.log(document.querySelector('h1'));
-
-// Multiple elemen
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByClassName('item'));
-
-const ul = document.querySelector('.items');
-
-// ul.remove();
-// ul.lastElementChild.remove()
-
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Juan';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
-
-const btn = document.querySelector('.btn');
-
-btn.style.background = 'red';
-
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('click');
-    document.querySelector('#my-form')
-    .style.background = '#ccc';
-
-    document.querySelector('body').
-    classList.add('bg-dark');
-
-    document.querySelector('.items')
-    .lastElem 
-    */
+}
